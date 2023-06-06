4f308e3244d2838085e9c9ccf3f476284e4a36b9
AnirbanChakraborty260106 <135621053+AnirbanChakraborty260106@users.noreply.github.com>
2023-06-06 03:50:00 -0700

    some changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,14 +9,14 @@ public class Main {
         System.out.printf("Hello and welcome anirban ashutosh!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 6; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
             System.out.println("i = " + i);
             System.out.println("i = " + i);
-            System.out.println("i = " + i);
+            String s = "hellow"; 
         }
     }
 }
